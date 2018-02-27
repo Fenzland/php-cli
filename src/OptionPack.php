b1c8866750bab6e83df002e744efffb355120bdd
Fenz <uukooo@126.com>
2018-02-27 21:09:37 +0800

    edit OptionPack carraies options with an array and provides basic exists() get() set() remove() methods

--- src/OptionPack.php
+++ src/OptionPack.php
@@ -8,5 +8,70 @@ namespace Fenzland\CLI;
 
 class OptionPack
 {
-	#
+
+	/**
+	 * Check if option exists.
+	 *
+	 * @param  string $option
+	 *
+	 * @return bool
+	 */
+	public function exists( string$option ):bool
+	{
+		return isset( $this->options[$option] );
+	}
+
+	/**
+	 * Get option value.
+	 *
+	 * @param  string ...$options
+	 *
+	 * @return mixed
+	 */
+	public function get( string...$options )
+	{
+		foreach( $options as $option )
+			if( $this->exists( $option ) )
+				return $this->options[$option];
+
+		return null;
+	}
+
+	/**
+	 * Set option value.
+	 *
+	 * @param  string $option
+	 * @param  mixed $value
+	 *
+	 * @return void
+	 */
+	public function set( string$option, $value ):void
+	{
+		$this->options[$option]= $value;
+	}
+
+	/**
+	 * Remove option.
+	 *
+	 * @param  string $option
+	 *
+	 * @return void
+	 */
+	public function remove( string$option ):void
+	{
+		if( $this->exists( $option ) )
+		{
+			unset( $this->options[$option] );
+		}
+	}
+
+	/**
+	 * Options.
+	 *
+	 * @access protected
+	 *
+	 * @var    array
+	 */
+	protected $options= [];
+
 }
